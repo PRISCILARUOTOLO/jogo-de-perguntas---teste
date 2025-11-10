8b90e74418c40e7aa90a322ef3b1d6a989089e0a
PRISCILA RUOTOLO <126161177+PRISCILARUOTOLO@users.noreply.github.com>
2025-11-10 09:48:02 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,1 +1,13 @@
+#Descrição responda 5 perguntas sobre Hollow Knight Silksong
+print("Jogo das Perguntas sobre Silksong")
+
+#Pergunta 1
+
+primeira = input("1)Qual é a região inicial do jogo? = ").strip().lower()
+if primeira == "Gruta" or primeira == "Moss":
+    print("Parabéns você acertou!")
+else:
+    print("Errado! Você não acertou")
+       
+
 
